25213a7ab21c1818c479a0e6a8948aceb1bc4c4e
Xiangwei Liu <xiangweimg@gmail.com>
2022-11-17 21:37:34 -0500

    11.17-1

--- db/migrate/20221115220347_changeproducts2.rb
+++ db/migrate/20221115220347_changeproducts2.rb
@@ -1,7 +1,7 @@
 class Changeproducts2 < ActiveRecord::Migration[7.0]
   def change
-    remove_column :products, :category
-    add_reference :products, :category, index: true, null: false
+    remove_column :products, :category, :string
+    add_reference :products, :category, index: true
     add_foreign_key :products, :categories
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -75,7 +75,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_15_220347) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "store_id", null: false
-    t.bigint "category_id", null: false
+    t.bigint "category_id"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["seller_id"], name: "index_products_on_seller_id"
     t.index ["store_id"], name: "index_products_on_store_id"

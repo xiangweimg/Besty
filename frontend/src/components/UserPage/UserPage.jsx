d329d5b0bac1f6069f5b5eb8b12b9fc546c31aee
Xiangwei Liu <xiangweimg@gmail.com>
2022-11-22 13:04:59 -0500

    11.22-4

--- frontend/src/components/UserPage/UserPage.jsx
+++ frontend/src/components/UserPage/UserPage.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
-import {  useSelector } from 'react-redux';
 
 function UserPage() {
-    const sessionUser = useSelector(state => state.session.user);
-    // let userId
-    // if(sessionUser){
-    //     userId = sessionUser.id
-    // }
 
-if(sessionUser){
     return ( 
       <div className='header-wrapper'>
           <h1>Hi</h1>
       </div>
       );
-      }
+      
 }
 
 export default UserPage;
